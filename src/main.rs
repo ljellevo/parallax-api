14a0d022a58ba43111ae3611a82d6e4ae85b4631
Ludvig Ellevold <21200123+ljellevo@users.noreply.github.com>
2020-07-10 17:37:14 +0200

    Added support for recieving images and middleware to validate request before coming in to API

--- src/main.rs
+++ src/main.rs
@@ -2,114 +2,129 @@
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use] extern crate serde;
+
+
+#[macro_use] 
+extern crate rocket;
+extern crate rocket_multipart_form_data;
+
+
+mod middleware;
+use crate::middleware::MultipartError;
+use crate::middleware::NewPayload;
+
+type Result<T> = std::result::Result<T, MultipartError>;
+
+
 
 use std::io::prelude::*;
 use std::fs::File;
 use serde::Serialize;
 use rocket_contrib::json::Json;
 use serde::Deserialize;
-use rocket::response::content;
+
+
+use rocket::Data;
+use rocket::http::ContentType;
+
+use rocket_multipart_form_data::{mime, MultipartFormDataOptions, MultipartFormData, MultipartFormDataField, Repetition};
+
 
 #[get("/hello")]
 fn hello() -> String {
-  format!("Hello")
+	format!("Hello")
 }
 
 #[derive(Serialize)]
 struct Task { 
-  data: String
+	data: String
 }
 
 #[get("/api/upcoming_features")]
 fn upcoming_features() -> Json<Task> {
-  let mut file = File::open("markdown/upcoming_features.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/upcoming_features.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/release_notes")]
 fn release_notes() -> Json<Task> {
-  let mut file = File::open("markdown/release_notes.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/release_notes.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/how_it_works")]
 fn how_it_works() -> Json<Task> {
-  let mut file = File::open("markdown/how_it_works.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/how_it_works.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/about_us")]
 fn about_us() -> Json<Task> {
-  let mut file = File::open("markdown/about_us.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/about_us.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/privacy")]
 fn privacy() -> Json<Task> {
-  let mut file = File::open("markdown/privacy.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/privacy.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/terms_of_agreement")]
 fn terms_of_agreement() -> Json<Task> {
-  let mut file = File::open("markdown/terms_of_agreement.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/terms_of_agreement.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
 #[get("/api/code_of_conduct")]
 fn code_of_conduct() -> Json<Task> {
-  let mut file = File::open("markdown/code_of_conduct.md").expect("Unable to open the file");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Unable to read the file");
-  Json(Task { data: contents})
+	let mut file = File::open("markdown/code_of_conduct.md").expect("Unable to open the file");
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).expect("Unable to read the file");
+	Json(Task { data: contents})
 }
 
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-struct Payload {
-  effect: String,
-  //image: String
-}
 
 #[derive(Serialize)]
 struct Output {
-  effect: String,
+	effect: String,
 }
 
-#[post("/api/image", format = "json", data = "<payload>")]
-fn upload_image(payload: Json<Payload>) -> Json<Output> {
-  //format!("print test {:?}", payload)
-  //content::Json("{ 'hi': 'world' }")
-  Json(
-    Output {
-        effect: payload.effect.to_string()
-    },
-  )
+//https://blog.krruzic.xyz/rocket-multipart/
+//https://docs.rs/rocket-multipart-form-data/0.9.3/rocket_multipart_form_data/
+#[post("/api/image", data = "<multipart>")]
+fn upload_image(multipart: Result<NewPayload>) -> String {
+  match multipart {
+    Ok(m) => format!("Hello, {}", m.payload.effect),
+    Err(e) => format!("Error: {}", e.reason),
+  }
 }
+
 
 fn main() {
-  rocket::ignite().mount("/", routes![
-      hello, 
-      upcoming_features,
-      release_notes,
-      how_it_works,
-      about_us,
-      privacy,
-      terms_of_agreement,
-      code_of_conduct,
-      upload_image
-    ]
-  ).launch();
+	rocket::ignite().mount("/", routes![
+		hello, 
+		upcoming_features,
+		release_notes,
+		how_it_works,
+		about_us,
+		privacy,
+		terms_of_agreement,
+		code_of_conduct,
+		upload_image
+		]
+	).launch();
 }
---
+++ src/middleware.rs
@@ -0,0 +1,131 @@
+use rocket::data::{FromDataSimple, Outcome};
+use rocket::http::Status;
+use rocket::{Data, Outcome::*, Request};
+use rocket_multipart_form_data::{mime, MultipartFormDataOptions, MultipartFormData, MultipartFormDataField, TextField, RawField, Repetition};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+pub struct MultipartError {
+	pub reason: String,
+}
+impl MultipartError {
+  fn new(reason: String) -> MultipartError {
+    MultipartError { reason }
+  }
+}
+impl std::fmt::Display for MultipartError {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "{}", self.reason)
+  }
+}
+
+/// simple representation of a user
+#[derive(Serialize, Deserialize)]
+pub struct Payload {
+	pub effect: String,
+}
+
+/// multipart form is loaded into this struct
+/// this is what's passed through to the route we'll create later
+pub struct NewPayload {
+  /// the submitted image
+  pub image: Vec<u8>,
+  /// we'll deserialize the json into a User
+  pub payload: Payload,
+}
+
+
+impl FromDataSimple for NewPayload {
+	type Error = MultipartError;
+
+	fn from_data(request: &Request, data: Data) -> Outcome<Self, Self::Error> {
+    let image_bytes;
+    let post_obj;
+    let mut options = MultipartFormDataOptions::new();
+
+    options.allowed_fields.push(
+      MultipartFormDataField::raw("image")
+        .size_limit(8 * 1024 * 1024) // 8 MB
+        .content_type_by_string(Some(mime::IMAGE_STAR))
+        .unwrap(),
+    );
+  
+    options.allowed_fields.push(MultipartFormDataField::text("data").content_type(Some(mime::STAR_STAR)));
+
+    let content_type = match request.content_type() {
+      Some(content_type) => content_type,
+      None => {
+        return Failure((
+          Status::BadRequest,
+          MultipartError::new(format!("Incorrect contentType, should be 'multipart/form-data")),
+        ))
+      }
+    };
+
+    let multipart_form = match MultipartFormData::parse(&content_type, data, options) {
+      Ok(m) => m,
+      Err(e) => {
+        return Failure((Status::BadRequest, MultipartError::new(format!("{:?}", e))))
+      }
+    };
+
+    let post_json_part = match multipart_form.texts.get("data") {
+      Some(post_json_part) => post_json_part,
+      _ => {
+          return Failure((
+            Status::BadRequest,
+            MultipartError::new(format!("Missing field 'data'")),
+        ))
+      }
+    };
+
+    let image_part: &Vec<RawField> = match multipart_form.raw.get("image") {
+      Some(image_part) => image_part,
+      _ => {
+        return Failure((
+          Status::BadRequest,
+          MultipartError::new(format!("Missing field 'image'")),
+        ))
+      }
+    };
+
+    match post_json_part.len() {
+      1 => {
+        let json_string = &post_json_part[0].text.replace('\'', "\"");
+        post_obj = match serde_json::from_str::<Payload>(json_string) {
+          Ok(insert) => insert,
+          Err(e) => {
+            return Failure((
+              Status::BadRequest,
+              MultipartError::new(format!("{:?}", e)),
+            ))
+          }
+        };
+      }
+      _ => {
+        return Failure((
+          Status::BadRequest,
+          MultipartError::new(format!("Extra text fields supplied")),
+        ))
+      } 
+    };
+      
+    match image_part.len() {
+      1 => {
+        image_bytes = image_part[0].raw.clone();
+      }
+      _ => {
+        return Failure((
+          Status::BadRequest,
+          MultipartError::new(format!("Extra image fields supplied")),
+        ))
+      }
+    };
+    
+
+    Success(NewPayload {
+      payload: post_obj,
+      image: image_bytes,
+    })
+	}
+}
